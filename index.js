a6b7bb1c894326d1c37e772d3b7e490d2fb1196b
Geovane Feitosa Cavalcante <geovanefeitosacavalcannte@gmail.com>
2018-12-18 09:21:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+
+const users = ['Geovane', 'Aquino', 'Gabriel']
+
+app.get('/', (req, res) => {
+  return res.render('list', { users })
+})
+
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  return res.redirect('/')
+})
+
+app.listen(8080)
